e4386f1bff19fba59efabddff0cca87d4f8db45a
nwporsch <31194304+nwporsch@users.noreply.github.com>
2019-10-02 12:55:40 -0500

    Basic Framework of how personalities work

--- Grand-Theft-Democracy.js
+++ Grand-Theft-Democracy.js
@@ -1,1 +1,30 @@
 /* Grand-Theft-Democracy.js Javascript */
+
+//Describes the party for a Candidate
+class Party = {
+    constructor(Partyname){
+      this.name = Partyname
+    }
+    this.personalites = [1,2,3]
+  }
+
+
+class Candidate{
+  constructor(firstname, lastname,party){
+    this.firstname = firstname,
+    this.lastname = lastname,
+    this.party = new Party(party),
+    this.personality = Math.floor(Math.random() * 3)
+  }
+}
+
+// Try edit message
+const data = {
+  minimum: 0,
+  message: Math.floor(Math.random() * (3) ),
+  party: "D"
+}
+
+$('#msg').html(personalities[data.message + data.minimum].toString(10))
+
+console.log(data)
