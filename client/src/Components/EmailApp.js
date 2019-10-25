09dec9b0cd2d4a150085d4e00a901bdfc998abda
Caleb Logan <catiel@ksu.edu>
2019-10-25 10:02:02 -0500

    Comments for EmailApp and EmailReader

--- client/src/Components/EmailApp.js
+++ client/src/Components/EmailApp.js
@@ -4,12 +4,14 @@ import EmailReader from './EmailReader.js';
 
 class EmailApp extends Component {
     constructor(props) {
-        super(props);
+		{/*Sets inital selected email to null and when an email is set to select it binds the selected email to be set.*/}
+		super(props);
         this.state = {
             selectedEmail: null
         }
         this.setSelectedEmail = this.setSelectedEmail.bind(this);
     }
+	{/*Renders the selected email, the email list and setting the selected email on a click.*/}
     render() {
         return(
             <div className="email-app"> 
@@ -18,10 +20,13 @@ class EmailApp extends Component {
                     selectedEmail={this.state.selectedEmail}
                     onSelect={this.setSelectedEmail}
                 />
+				
+				{/*Pulls inn the selected email from EmailReader.js*/}
                 <EmailReader email={this.state.selectedEmail}/>
             </div>
         )
     }
+	{/*function to set the currently selected email to be displayed in EmailReader.js*/}
     setSelectedEmail(email) {
         this.setState({selectedEmail: email}); //render gets retriggered as soon as state is changed
     }
