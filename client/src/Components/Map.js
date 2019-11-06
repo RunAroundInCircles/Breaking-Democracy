aecf920590948743c6bd6c7015d33f3bd42be6ee
Nickalas Porsch <porschnickalas@gmail.com>
2019-11-06 11:14:15 -0600

    Click events work!!

--- client/src/Components/Map.js
+++ client/src/Components/Map.js
@@ -2,12 +2,25 @@ import React,{Component} from 'react';
 import democristan from './Maps/country.svg.js';
 
 class Map extends Component{
-	
+
+	constructor(props) {
+		super(props);
+		this.mapRef = React.createRef();
+	}
+
+	componentDidMount() {
+		this.mapRef.current.querySelectorAll('g > path').forEach((district) => {
+			district.addEventListener('click', function(event){
+				district.style.fill = "orange";
+			})
+		});
+	}
+
 	render(){
 		const svg = {
 			__html: democristan
 		}
-		return <div dangerouslySetInnerHTML={svg}/>;
+		return <div dangerouslySetInnerHTML={svg} ref={this.mapRef} />
 	}
 }
 export default Map
