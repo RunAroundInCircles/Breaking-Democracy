40b41eef804a16af69439e046f152b90f5aa9357
Caleb Logan <catiel@ksu.edu>
2019-12-09 10:02:12 -0600

    fixed echo files

--- application/src/Components/Echo/Echo.js
+++ application/src/Components/Echo/Echo.js
@@ -8,16 +8,16 @@ class Echo extends Component{
             name: props.name, //Name of user
             body: props.body, //Body of the tweet
             at: props.at, //@ of the user
-            time: props.time, //Time of tweet
+            time: props.time //Time of tweet
         }
 	}
 	render() {
 		return(
-			<div className="email-reader">
-                <h1 className="echoh1">{this.props.state.name}</h1>
-                <h2 className="echoh2">{this.props.state.at}</h2>
-                <h3 className="echoh3">{this.props.state.time}</h3>
-                <body className="echobody">{this.props.email.body}</body>
+			<div className="echolist">
+                <h1 className="echoh1">{this.state.name}</h1>
+                <h2 className="echoh2">{this.state.at}</h2>
+                <h3 className="echoh3">{this.state.time}</h3>
+                <body className="echobody">{this.state.body}</body>
             </div>
 			
 		)
