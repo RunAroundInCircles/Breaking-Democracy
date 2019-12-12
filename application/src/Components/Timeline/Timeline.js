329d8dc5cb573853d3d95da7bc83881ce544675d
GavinMNeises <gmneises@ksu.edu>
2019-12-12 17:25:44 -0600

    Added timeline svg

--- application/src/Components/Timeline/Timeline.js
+++ application/src/Components/Timeline/Timeline.js
@@ -4,7 +4,23 @@ class Timeline extends Component{
 	
 	render(){
 		return(
-			<div>
+			<div 
+				style={{display: 'flex',
+				alignItems: 'center',
+				justifyContent: 'center',
+				position: 'absolute',
+				width: '100vw',
+				height: '80vh'
+        }}>
+				<svg height="50" width="50">
+  					<polygon points="0,25 50,0 50,50" style={{fill: 'grey', stroke: 'red', strokeWidth: 3}}/>
+				</svg>
+				<svg height="50" width="100vw">
+  					<line x1="0" y1="25" x2="100vw" y2="25" style={{stroke: 'red', strokeWidth:3}} />
+				</svg>
+				<svg height="50" width="50">
+  					<polygon points="50,25 0,0 0,50" style={{fill: 'grey', stroke: 'red', strokeWidth: 3}}/>
+				</svg>
 			</div>
 		)
 	}
