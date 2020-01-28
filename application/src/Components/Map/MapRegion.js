cd10e46c83cdf0b98ea9004c8dec64989ff77193
GavinMNeises <gmneises@ksu.edu>
2020-01-28 17:06:47 -0600

    Merging

--- application/src/Components/Map/MapRegion.js
+++ application/src/Components/Map/MapRegion.js
@@ -1,18 +1,39 @@
-import React,{Component} from 'react';
-import region1 from '../../Resources/Maps/Region-1.svg.js';
-import region2 from '../../Resources/Maps/Region-2.svg.js';
-import region3 from '../../Resources/Maps/Region-3.svg.js';
-import region4 from '../../Resources/Maps/Region-4.svg.js';
-import region5 from '../../Resources/Maps/Region-5.svg.js';
-import region6 from '../../Resources/Maps/Region-6.svg.js';
-import region7 from '../../Resources/Maps/Region-7.svg.js';
-import region8 from '../../Resources/Maps/Region-8.svg.js';
-
-
+import React from 'react';
+import {Link} from "react-router-dom";
+import {Button} from 'react-bootstrap';
 
 function MapRegion(props) {
     return (
-        <span>Banana</span>
+        <div style={{display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            position: 'fixed',
+            width: '100vw',
+            height: '100vh',
+            zIndex: 1000,
+            top: 0,
+            left: 0,
+            backgroundColor: 'rgba(0, 0, 0, .7)'
+        }}>
+            {/*This div is the body of the popup window containing the back button and the event info*/}
+            <div style={{backgroundColor: 'rgba(255, 255, 255, 1)',
+                borderRadius: '5px',
+                width: '50vw',
+                height: '50vh',
+                position: 'absolute'
+            }}>
+                {/*This link acts as a back button allowing the user to redirect to Calendar*/}
+                <Link to='/Map' >
+                    <Button style={{top: 5, right: 5, position: 'absolute'}}>
+						<span>X</span>
+					</Button>
+                </Link>
+                {/*This div contains all of the information for the event*/}
+                <div style={{justifyContent: 'center'}}>
+                    {/*Put map here*/}
+                </div>
+            </div>
+        </div>
     );
 }
 
