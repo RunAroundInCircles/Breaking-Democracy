c24f4dd408eda7547127356da5501e5c7151ddda
Caleb Logan <catiel@ksu.edu>
2020-02-10 22:33:30 -0600

    fix test!

--- application/src/Components/Email/Email.test.js
+++ application/src/Components/Email/Email.test.js
@@ -3,7 +3,7 @@ import EmailApp from './EmailApp.js';
 import emails from './EmailList.json';
 import MainPage from '../../App.js';
 import renderer from 'react-test-renderer';
-
+import { shallow } from 'enzyme';
 import ReactDOM from 'react-dom';
 
 /**
@@ -13,4 +13,20 @@ test('renders emailApp page', () => {
 	const tree = renderer
 	.create(<EmailApp emails={emails}/>)
 	expect(tree).toMatchSnapshot();
+});
+
+
+test('renders each email', () => {
+	const tree = renderer
+	.create(<EmailApp emails={emails}/>)
+	.toJSON();
+	expect(tree.state).not.toBeNull();
+});
+
+
+test('renders each email', () => {
+	const wrapper = shallow(<EmailApp emails={emails}/>);
+	const button = wrapper.find('selected-email');
+	button.simulate('click');
+	expect(wrapper.state.onSelect).not.toBeNull();
 });
