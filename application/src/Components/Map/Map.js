42be53094e18a05ff0f8d61b82c5149f2a4ffa3c
GavinMNeises <gmneises@ksu.edu>
2020-02-11 00:07:07 -0600

    Commented Map and MapApp

--- application/src/Components/Map/Map.js
+++ application/src/Components/Map/Map.js
@@ -13,16 +13,17 @@ class Map extends Component{
 	constructor(props) {
 		super(props);
 		this.state = {
-			regions: {
-				0: {red:10, blue:90},
-				1: {red:10, blue:90},
-				2: {red:10, blue:90},
-				3: {red:10, blue:90},
-				4: {red:10, blue:90},
-				5: {red:10, blue:90},
-				6: {red:10, blue:90},
-				7: {red:10, blue:90},
-				8: {red:10, blue:90}
+			//regions stores the color information for all of the different regions in the map
+			regions: { 
+				0: {red: 255 * Math.random(), blue: 255 * Math.random()},
+				1: {red: 255 * Math.random(), blue: 255 * Math.random()},
+				2: {red: 255 * Math.random(), blue: 255 * Math.random()},
+				3: {red: 255 * Math.random(), blue: 255 * Math.random()},
+				4: {red: 255 * Math.random(), blue: 255 * Math.random()},
+				5: {red: 255 * Math.random(), blue: 255 * Math.random()},
+				6: {red: 255 * Math.random(), blue: 255 * Math.random()},
+				7: {red: 255 * Math.random(), blue: 255 * Math.random()},
+				8: {red: 255 * Math.random(), blue: 255 * Math.random()}
 			}
 		}
 
@@ -30,8 +31,8 @@ class Map extends Component{
 	}
 
 	/**
-	 * Shows the map referenced by currentId
-	 * @return {Div} Displays the current region or map based off the specific ID
+	 * Shows the country map
+	 * @return {Div} Displays the country map
 	 */
 	render(){
 		var svg = {
@@ -42,12 +43,16 @@ class Map extends Component{
 
 	/**
 	 * Attaches onSelect click event to all regions of the map
-	 * @return {[Function]} The Function will be used to display the specified region or based off the ID given
+	 * @return {[Function]} The Function will be used to redirect to the regional map
 	 */
 	componentDidMount() {
 		var func = this.props.onSelect; //Allows access to onSelect within the forEach
-		//Iterates through all paths within the svg and attaches the click event to each path
+		var colors = this.state.regions; //Allows access to regions within the forEach
+		//Iterates through all paths within the svg and attaches the click event to each path and gives them their unique color
 		this.mapRef.current.querySelectorAll('g > path').forEach((region, index) => {
+			var color = 'rgb(' + colors[index].red + ', 0, ' + colors[index].blue + ')'; //Formats colors to be used
+			region.style.setProperty("fill", color);
+			region.style.setProperty("fill:hover", 'gold');
 			region.addEventListener('click', function(event) {
 				//Decides which region map to show based on what region is clicked on
 				func(index);			
