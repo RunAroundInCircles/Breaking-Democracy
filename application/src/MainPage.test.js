d3531e17aa887a83a2d2b6ca5eedb6e3cbe7bf9c
Caleb Logan <catiel@ksu.edu>
2020-02-10 20:19:32 -0600

    Sucessful test pass

--- application/src/MainPage.test.js
+++ application/src/MainPage.test.js
@@ -4,9 +4,16 @@ import Calendar from './Components/Calendar/Calendar.js';
 import MainPage from './MainPage.js';
 import renderer from 'react-test-renderer';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Calendar />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test('renders calendar page', () => {
+	const tree = renderer
+	.create(<Router><Link page="http://localhost:3000/Calendar">Calendar</Link> </Router>)
+	.toJSON();
+	expect(tree).toMatchSnapshot();
 });
