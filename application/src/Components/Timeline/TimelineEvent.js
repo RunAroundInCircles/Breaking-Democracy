c1d32d33fd48da24ee5f6f2dcf21301b3664e12c
GavinMNeises <gmneises@ksu.edu>
2020-02-23 19:53:32 -0600

    Added History Bubbles to Timeline

--- application/src/Components/Timeline/TimelineEvent.js
+++ application/src/Components/Timeline/TimelineEvent.js
@@ -10,8 +10,8 @@ class TimelineEvent extends Component {
         super(props);
 
         this.state = {
-            //timelineevents contains all the events on the timeline given by TimelineApp
-            timelineevents: props.timelineevents
+            //timelineEvents contains all the events on the timeline given by TimelineApp
+            timelineEvents: props.timelineEvents
         }
     }
 	/**
@@ -20,15 +20,41 @@ class TimelineEvent extends Component {
  	*/
     render() {
         //The following mapping function finds the email that the user selected
-        const timelineevents = this.props.timelineevents.map((one_event) => {
-            return (<Timeline key={one_event.name}
-                {...one_event}/>);
-        });
+        var eventsFormated = [];
+        var i;
+        for(i = 0; i < this.props.timelineEvents.length; i++) {
+            var percent = (Math.random() * 2) - 1;
+            percent = percent.toFixed(2);
+            var fillColor = "grey";
+            if(percent > 0) {
+                percent = "+" + percent;
+                fillColor = "green";
+            }
+            else if(percent < 0) {
+                fillColor = "red";
+            }
+            var tooltipText =  this.props.timelineEvents[i].date + "\n" + this.props.timelineEvents[i].name + "\n" + this.props.timelineEvents[i].description;
+            eventsFormated.push(
+                <svg height="100" width="100" style={{margin: 4}}>
+                    <circle cx="50" cy="50" r="47" stroke="black" strokeWidth="3" fill={fillColor}>
+                        <title>{tooltipText}</title>
+                    </circle>
+                    <text x="50" y="50" font-family="sans-serif" font-size="20px" fill="white" text-anchor="middle">{percent}</text>
+                </svg>
+            );
+        }
+
         return(
-            <div className="timeline-list">
-                {/**/}
-                <h1 className="Event-head">Timeline</h1>
-                {timelineevents}
+            <div className="timeline-list"
+                style={{display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    position: 'absolute',
+                    width: '100vw',
+                    height: '80vh'
+                }}
+            >
+                {eventsFormated}
             </div>
         )
     }
