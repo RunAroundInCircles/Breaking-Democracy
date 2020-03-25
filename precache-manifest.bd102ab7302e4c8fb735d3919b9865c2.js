d42303b82a50f6b71ab30b758140356693762490
Caleb Logan <31215887+catiel@users.noreply.github.com>
2020-03-25 14:33:39 -0500

    Updates

---
+++ precache-manifest.bd102ab7302e4c8fb735d3919b9865c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f3b530f935b5941d037f3b3c277f987",
+    "url": "/Breaking-Democracy/index.html"
+  },
+  {
+    "revision": "c51c8c0ef94417f1216b",
+    "url": "/Breaking-Democracy/static/css/main.4a016d26.chunk.css"
+  },
+  {
+    "revision": "13df74077752784b4d37",
+    "url": "/Breaking-Democracy/static/js/2.19bbc2a5.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/Breaking-Democracy/static/js/2.19bbc2a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c51c8c0ef94417f1216b",
+    "url": "/Breaking-Democracy/static/js/main.0a92ceb6.chunk.js"
+  },
+  {
+    "revision": "19f78b4f8a673c67f7ba",
+    "url": "/Breaking-Democracy/static/js/runtime-main.420d1e7d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Breaking-Democracy/application/src/precache-manifest.2282d85f944e1020a4ef7c34b934cada.js"
+  "/Breaking-Democracy/precache-manifest.bd102ab7302e4c8fb735d3919b9865c2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Breaking-Democracy/application/src/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Breaking-Democracy/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
