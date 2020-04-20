aa68661403dbc0e542202ca80b6161910ae721eb
GavinMNeises <gmneises@ksu.edu>
2020-04-19 20:28:59 -0500

    Fixed all of the tests and updated their comments

--- application/src/Components/Map/Map.js
+++ application/src/Components/Map/Map.js
@@ -56,25 +56,28 @@ class Map extends Component{
 	 */
 	componentDidMount() {
 		var func = this.props.onSelect; //Allows access to onSelect within the forEach
-		//Iterates through all paths within the svg and attaches the click event to each path and gives them their unique color
-		this.mapRef.current.querySelectorAll('g > path').forEach((region, index) => {
-			var i;
-			var red = 0;
-			var blue = 0;
-			for(i = 0; i < this.props.pollData[index].length; i++) {
-				red += this.props.pollData[index][i];
-			}
-			red = red/this.props.pollData[index].length;
-			blue = 100-red;
-			
-			var color = 'rgb(' + (red/100)*255 + ', 0, ' + (blue/100)*255 + ')'; //Formats colors to be used
-			region.style.setProperty("fill", color);
-			region.style.setProperty("fill:hover", 'gold');
-			region.addEventListener('click', function(event) {
-				//Decides which region map to show based on what region is clicked on
-				func(index);			
-			})
-		});
+		//Ensures that mapRef.current isn't null
+		if(this.mapRef.current) {
+			//Iterates through all paths within the svg and attaches the click event to each path and gives them their unique color
+			this.mapRef.current.querySelectorAll('g > path').forEach((region, index) => {
+				var i;
+				var red = 0;
+				var blue = 0;
+				for(i = 0; i < this.props.pollData[index].length; i++) {
+					red += this.props.pollData[index][i];
+				}
+				red = red/this.props.pollData[index].length;
+				blue = 100-red;
+				
+				var color = 'rgb(' + (red/100)*255 + ', 0, ' + (blue/100)*255 + ')'; //Formats colors to be used
+				region.style.setProperty("fill", color);
+				region.style.setProperty("fill:hover", 'gold');
+				region.addEventListener('click', function(event) {
+					//Decides which region map to show based on what region is clicked on
+					func(index);			
+				})
+			});
+		}
 	}
 }
 export default Map;
