fa0278478efc474629ff5dac16f0bd4b8ae87768
Caleb Logan <31215887+catiel@users.noreply.github.com>
2020-04-21 14:23:38 -0500

    Updates

---
+++ precache-manifest.600ed09939374c18300ebfefdb5e228b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04d128e585319698e56f483eb50778d7",
+    "url": "/Breaking-Democracy/index.html"
+  },
+  {
+    "revision": "8403c9b4fafc167acff3",
+    "url": "/Breaking-Democracy/static/css/main.fba3b5e0.chunk.css"
+  },
+  {
+    "revision": "0b41e59f13e2ffbd1892",
+    "url": "/Breaking-Democracy/static/js/2.b60eabf0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Breaking-Democracy/static/js/2.b60eabf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8403c9b4fafc167acff3",
+    "url": "/Breaking-Democracy/static/js/main.b87b74ef.chunk.js"
+  },
+  {
+    "revision": "8dc9abc20ee8c0017995",
+    "url": "/Breaking-Democracy/static/js/runtime-main.b7e529be.js"
+  },
+  {
+    "revision": "085a765a83351ac4611fb530bf206b0d",
+    "url": "/Breaking-Democracy/static/media/ThemeLoopable.085a765a.mp3"
+  },
+  {
+    "revision": "085a765a83351ac4611fb530bf206b0d",
+    "url": "/Breaking-Democracy/static/media/ThemeLoopable.085a765a.wav"
+  },
+  {
+    "revision": "95844bda8d3a7a8b0deaec403e8dda61",
+    "url": "/Breaking-Democracy/static/media/intro.95844bda.mp4"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Breaking-Democracy/precache-manifest.d321bbf8250d0485a6ee27bd13b859cd.js"
+  "/Breaking-Democracy/precache-manifest.600ed09939374c18300ebfefdb5e228b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Breaking-Democracy/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
