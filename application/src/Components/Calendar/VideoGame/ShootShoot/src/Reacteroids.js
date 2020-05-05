bb9a49139b03cffda78f5e5351c398eaa81dd43a
Nickalas Porsch <porschnickalas@gmail.com>
2020-05-05 15:33:48 -0500

    Made game music quieter

--- application/src/Components/Calendar/VideoGame/ShootShoot/src/Reacteroids.js
+++ application/src/Components/Calendar/VideoGame/ShootShoot/src/Reacteroids.js
@@ -33,6 +33,7 @@ export class Reacteroids extends Component {
         ratio: window.devicePixelRatio || 1,
       },
       context: null,
+      //The key array holds onto whether any of the keys are pressed (0 if not pressed, 1 if pressed)
       keys : {
         left  : 0,
         right : 0,
@@ -40,12 +41,13 @@ export class Reacteroids extends Component {
         down  : 0,
         space : 0,
       },
-      asteroidCount: 3,
-      currentScore: 0,
-      topScore: localStorage['topscore'] || 0,
-      inGame: false,
-      playMusic: true
+      asteroidCount: 3, // The number of initial asteroids at the beginning of the round
+      currentScore: 0, // The player's score
+      topScore: localStorage['topscore'] || 0, // The local highscore (highscores are saved on the player's local machine)
+      inGame: false, // Determines if the player is in game
+      playMusic: true // Determines if the game music should be played
     }
+
 	this.result = 0;
     this.ship = [];
     this.asteroids = [];
@@ -53,6 +55,11 @@ export class Reacteroids extends Component {
     this.particles = [];
   }
 
+/**
+ * Resizes the game if the window is resized
+ * @param  {[type]} value Value from the event handler
+ * @param  {[type]} e     Event Handler
+ */
   handleResize(value, e){
     this.setState({
       screen : {
@@ -63,6 +70,12 @@ export class Reacteroids extends Component {
     });
   }
 
+/**
+ * Handles user input from the user and tells if the player has pressed a key
+ * @param  {[type]} value Determines if the key was pressed
+ * @param  {[type]} e     The event handler for the eky
+ * @return {[type]}       Sets the keys array to the current values of the keys
+ */
   handleKeys(value, e){
     let keys = this.state.keys;
     if(e.keyCode === KEY.LEFT   || e.keyCode === KEY.A) keys.left  = value;
@@ -74,28 +87,43 @@ export class Reacteroids extends Component {
     });
   }
 
+/**
+ * Adds event listeners to check if a key is pressed. Adds the canvas for the game to the context
+ * Starts the game and starts animation
+ */
   componentDidMount() {
+    // Event listeners used to tell if keys have been pressed or the window has been resized
     window.addEventListener('keyup',   this.handleKeys.bind(this, false));
     window.addEventListener('keydown', this.handleKeys.bind(this, true));
     window.addEventListener('resize',  this.handleResize.bind(this, false));
 
+    // Creates a canvas with 2d graphics
     const context = this.refs.canvas.getContext('2d');
     this.setState({ context: context });
+    // Starts the game
     this.startGame();
+    // Allows for frames to be changed which creates animation
     requestAnimationFrame(() => {this.update()});
   }
 
+  /**
+   * Removes the eventlisters from the window
+   */
   componentWillUnmount() {
     window.removeEventListener('keyup', this.handleKeys);
     window.removeEventListener('keydown', this.handleKeys);
     window.removeEventListener('resize', this.handleResize);
   }
 
+/**
+ * Updates animation
+ */
   update() {
-    const context = this.state.context;
-    const keys = this.state.keys;
+    const context = this.state.context; //Retrieves the canvas
+    const keys = this.state.keys; //Determines which keys have been pressed
     const ship = this.ship[0];
 
+    //Saves the current canvas and scales the canvas to the screen
     context.save();
     context.scale(this.state.screen.ratio, this.state.screen.ratio);
 
@@ -107,35 +135,46 @@ export class Reacteroids extends Component {
 
     // Next set of asteroids
     if(!this.asteroids.length){
-      let count = this.state.asteroidCount + 1;
-      this.setState({ asteroidCount: count });
-      this.generateAsteroids(count)
+      let count = this.state.asteroidCount + 1; // Creates more asteroids if needed
+      this.setState({ asteroidCount: count }); // Updates the count of the asteroids
+      this.generateAsteroids(count) // Animates the asteroids
     }
 
     // Check for colisions
-    this.checkCollisionsWith(this.bullets, this.asteroids);
-    this.checkCollisionsWith(this.ship, this.asteroids);
+    this.checkCollisionsWith(this.bullets, this.asteroids); // Check if bullets have hit an asteroid
+    this.checkCollisionsWith(this.ship, this.asteroids); // Check if asteroids have hit player
 
     // Remove or render
-    this.updateObjects(this.particles, 'particles')
-    this.updateObjects(this.asteroids, 'asteroids')
-    this.updateObjects(this.bullets, 'bullets')
-    this.updateObjects(this.ship, 'ship')
+    this.updateObjects(this.particles, 'particles') // Changes position of particles or removes them
+    this.updateObjects(this.asteroids, 'asteroids') // Changes position of asteroids, breaks them, or removes them
+    this.updateObjects(this.bullets, 'bullets') // Changes position of bullets or removes them
+    this.updateObjects(this.ship, 'ship') // Changes position of ship or removes ship
 
+    // Updates canvas to the new animations
     context.restore();
 
-    // Next frame
+    // Creates the next frame of animation
     requestAnimationFrame(() => {this.update()});
   }
 
+/**
+ * If the player has scored points update the score
+ * @param {[type]} points The number of points the player has just earned
+ */
   addScore(points){
+    // Checks if the player is still in game
     if(this.state.inGame){
+      // Updates the players score
       this.setState({
         currentScore: this.state.currentScore + points,
       });
     }
   }
 
+/**
+ * Starts the game, stops the main music playing, begins intial placement of objects
+ * @return {[type]} [description]
+ */
   startGame(){
     this.setState({
       inGame: true,
@@ -145,30 +184,37 @@ export class Reacteroids extends Component {
     //Disable main music loop
     this.props.updateMainMusic(false);
 
-    // Make ship
+    //Set the volume of the game to a lower volume
+    var audio = document.getElementById("game-music");
+    audio.volume = 0.5;
+    // Creates the new ship
     let ship = new Ship({
       position: {
         x: this.state.screen.width/2,
         y: this.state.screen.height/2
       },
-      create: this.createObject.bind(this),
-      onDie: this.gameOver.bind(this)
+      create: this.createObject.bind(this), // Allows for the ship to be able to create other objects
+      onDie: this.gameOver.bind(this) // If the player dies then go to gameOver
     });
-    this.createObject(ship, 'ship');
+    this.createObject(ship, 'ship'); // Create the intial ship
 
     // Make asteroids
     this.asteroids = [];
     this.generateAsteroids(this.state.asteroidCount)
   }
 
+  /**
+   * If the player has died then the player is no longer in game.
+   * Renable main music and determine the players influence based on their score in the asteroids game.
+   */
   gameOver(){
     this.setState({
       inGame: false,
     }
 
   );
-  
-  //Reenable main music and disable game Music
+
+  //Re-enable main music and disable game Music
   this.props.updateMainMusic(true);
   this.setState({playMusic : false});
 
@@ -187,6 +233,10 @@ export class Reacteroids extends Component {
 	  this.props.callbackFromMain(this.props.eventID, percent);
   }
 
+  /**
+   * Creates the asteroids for the game
+   * @param  {[type]} howMany How many asteroids will be rendered
+   */
   generateAsteroids(howMany){
     let asteroids = [];
     let ship = this.ship[0];
@@ -194,8 +244,8 @@ export class Reacteroids extends Component {
       let asteroid = new Asteroid({
         size: 80,
         position: {
-          x: randomNumBetweenExcluding(0, this.state.screen.width, ship.position.x-60, ship.position.x+60),
-          y: randomNumBetweenExcluding(0, this.state.screen.height, ship.position.y-60, ship.position.y+60)
+          x: randomNumBetweenExcluding(0, this.state.screen.width, ship.position.x-60, ship.position.x+80),
+          y: randomNumBetweenExcluding(0, this.state.screen.height, ship.position.y-60, ship.position.y+80)
         },
         create: this.createObject.bind(this),
         addScore: this.addScore.bind(this)
@@ -250,7 +300,13 @@ export class Reacteroids extends Component {
     let endgame;
     let message;
 
-
+    //Find the game-music element and decrease it's volume to 0.2
+    var audio = document.getElementById("game-music");
+    if(audio != null){
+      if(audio.volume != 0.2){
+        audio.volume = 0.2
+      }
+    }
 
 
     if (this.state.currentScore <= 0) {
