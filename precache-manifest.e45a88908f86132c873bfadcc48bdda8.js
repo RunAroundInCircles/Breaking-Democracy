60724b4e10af927ea5db3e4106f69089fe353263
Caleb Logan <31215887+catiel@users.noreply.github.com>
2020-05-04 21:20:00 -0500

    Updates

---
+++ precache-manifest.e45a88908f86132c873bfadcc48bdda8.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f98c5247f9d3dadd956075ecab24554",
+    "url": "/Breaking-Democracy/index.html"
+  },
+  {
+    "revision": "056ed1cfc4f726bcf4c0",
+    "url": "/Breaking-Democracy/static/css/main.53e13693.chunk.css"
+  },
+  {
+    "revision": "6d876b61233043a592f3",
+    "url": "/Breaking-Democracy/static/js/2.7c9c4aab.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Breaking-Democracy/static/js/2.7c9c4aab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "056ed1cfc4f726bcf4c0",
+    "url": "/Breaking-Democracy/static/js/main.890383bf.chunk.js"
+  },
+  {
+    "revision": "8dc9abc20ee8c0017995",
+    "url": "/Breaking-Democracy/static/js/runtime-main.b7e529be.js"
+  },
+  {
+    "revision": "5bc2f6b807d745abf117931d341d41f7",
+    "url": "/Breaking-Democracy/static/media/AsteroidsLoopable.5bc2f6b8.wav"
+  },
+  {
+    "revision": "abc8a47583ba321e695e98860fb81131",
+    "url": "/Breaking-Democracy/static/media/AsteroidsLoopable.abc8a475.mp3"
+  },
+  {
+    "revision": "0836aef3f5cc2159320129eccc04168c",
+    "url": "/Breaking-Democracy/static/media/ThemeLoopable.0836aef3.wav"
+  },
+  {
+    "revision": "242ac998bc951f91c58b9b33af833bd8",
+    "url": "/Breaking-Democracy/static/media/ThemeLoopable.242ac998.mp3"
+  },
+  {
+    "revision": "0ead22b88bef6622bc67a6c4fb2fbda3",
+    "url": "/Breaking-Democracy/static/media/failurevid.0ead22b8.mp4"
+  },
+  {
+    "revision": "b5d1edede8759d3ed61316b0de08aba1",
+    "url": "/Breaking-Democracy/static/media/fan.b5d1eded.png"
+  },
+  {
+    "revision": "8e12ee6d771a8093f301308c69306d2a",
+    "url": "/Breaking-Democracy/static/media/game_master.8e12ee6d.png"
+  },
+  {
+    "revision": "95844bda8d3a7a8b0deaec403e8dda61",
+    "url": "/Breaking-Democracy/static/media/intro.95844bda.mp4"
+  },
+  {
+    "revision": "34f949450fde141634dacc3aa7e39259",
+    "url": "/Breaking-Democracy/static/media/putin.34f94945.png"
+  },
+  {
+    "revision": "df0cc616af992aa3069c1bacda3e67a8",
+    "url": "/Breaking-Democracy/static/media/victoryvid.df0cc616.mp4"
+  }
+]);
